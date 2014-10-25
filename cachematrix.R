4b834752af09879762eed106557e0e46253a6572
Aditya Singh <singh.r.aditya@gmail.com>
2014-10-25 15:46:29 -0700

    moving updated cachematrix.R to repo

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    dimx<- dim(x)
+    i <- matrix(rep(NA), nrow =dimx[1], ncol = dimx[2])
+    setmatrix <- function(y){
+        ## stuff
+        x<<- y
+        i <- matrix(rep(NA), nrow =dimx[1], ncol = dimx[2])
+    }
+    getmatrix <- function () {
+        x
+    }
+    setInv <- function (Inversem) {
+      i <<-Inversem  
+      
+    }
+    getInv <- function () {
+        li <-matrix(rep(NA), nrow =dimx[1], ncol = dimx[2])
+        if(identical(i,li)) return(NULL)
+        else return(i)
+    }
+    list(setmatrix = setmatrix, getmatrix = getmatrix,
+         setInv = setInv,
+         getInv = getInv)
+        
 }
 
 
@@ -12,4 +34,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getInv()    
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$getmatrix()
+    i <- solve(data, ...)
+    x$setInv(i)
+    i
 }
